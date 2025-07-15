2f146027567dc4da9222545a900e05551c0d5857
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-15 17:48:06 -0500

    ng lint and add new eslint

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+import { Component, Input, SimpleChanges, inject, signal, OnInit, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
-import { HeaderComponent } from '@shared/components/header/header.component';
+
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
@@ -11,10 +11,10 @@ import { Category } from '@shared/models/category.model';
 
 @Component({
     selector: 'app-list',
-    imports: [CommonModule, ProductComponent, HeaderComponent, RouterLinkWithHref],
+    imports: [CommonModule, ProductComponent, RouterLinkWithHref],
     templateUrl: './list.component.html'
 })
-export default class ListComponent {
+export default class ListComponent implements OnInit, OnChanges {
 
   products = signal<Product[]>([]);
   categories = signal<Category[]>([]);
@@ -28,6 +28,7 @@ export default class ListComponent {
   }
 
   ngOnChanges(changes: SimpleChanges) {
+    console.log('changes', changes);
     this.getProducts();
   }
 
@@ -41,9 +42,6 @@ export default class ListComponent {
       next: (products) => {
         this.products.set(products);
       },
-      error: () => {
-        
-      }
     })
   }
 
@@ -52,9 +50,6 @@ export default class ListComponent {
     .subscribe({
       next: (data) => {
         this.categories.set(data);
-      },
-      error: () => {
-        
       }
     })
   }
